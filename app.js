4d98beaeb27aca6ba3592dc8c662375bc3bd2392
Michael <94140658+Michael-Dlamini@users.noreply.github.com>
2022-02-11 23:00:37 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('./routes/testimonialapi');
+const app = express();
+const port = process.env.PORT || 5001
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//calling path
+app.use('/',router);
+
+
+//listing port
+//app.listen(port,()=> console.log('listen on port '+ port))
+app.listen(port,() => console.log(`Server running on port: http://localhost:${port}`));
